06f9fe72baf980b755e437d07623b38323e79377
Noel Maersk <veox+gits@veox.pw>
2019-02-04 00:39:55 +0200

    monitor: handle/print ommer and parent block.
    SQUASHED:
    
    * monitor: handle/print parent of block with ommers, too.
    
    Even though this will introduce some repetition (of blocks that were
    first seen as "the only option" to having become ommers), this seems
    like a net-positive, since some blocks (that previously went "unreported"
    but later were) will now be printed at least once.
    
    * monitor: print the number of ommers.
    
    The tree is not well;
    that is why we have numbers:
    numbers are perfect.
    
    * monitor: specify `ommertext` optionally in call to handle_new_block().
    
    This allows differentiating between parent and ommer of latest block
    while processing both.

--- monitor.py
+++ monitor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-# Monitor `extradata` field in ethereum blocks (canonical and linked ommers)
-# for progpow preference signal.
+# Monitor `extradata` field in Ethereum blocks (canonical and linked ommers)
+# for ProgPoW preference signal.
 #
 # Based on example: https://web3py.readthedocs.io/en/stable/filters.html#synchronous        
 
@@ -22,8 +22,8 @@ def detect_progpow_vote(extradata, nbytes=MAX_SCAN):
     try:
         scan = to_text(extradata[:nbytes])
     except UnicodeDecodeError as err:
-        print('Error while detecting vote!')
-        pp(err.args)
+        # print('Error while detecting vote!')
+        # pp(err.args)
         badpos = err.args[2]
         # second pass, more constrained
         return detect_progpow_vote(extradata, nbytes=badpos)
@@ -40,23 +40,40 @@ def detect_progpow_vote(extradata, nbytes=MAX_SCAN):
 
     return vote, nbytes
 
-def handle_new_block(blockhash):
+def handle_new_block(blockhash, recursive_call=False, ommertext=None):
+    if not ommertext:
+        if not recursive_call:
+            ommertext = '│'
+        else:
+            ommertext = '┝'
+
     block = w3.eth.getBlock(blockhash)
+
     blocknum = block['number']
     extradata = block['extraData']
+    ommers = block['uncles']
+    parent = block['parentHash']
 
     vote, safechars = detect_progpow_vote(extradata)
 
     try:
         extratext = to_text(extradata[:safechars])
     except UnicodeDecodeError as err:
-        print('Error while preparing extratext!')
-        pp(err.args)
+        # print('Error while preparing extratext!')
+        # pp(err.args)
         extratext = ''
 
-    print(blocknum, to_hex(blockhash), vote, extratext)
+    # NOTE: handling (printing) ommers before printing non-ommer block info
+    # (so that ommers are printed first)
+    if not recursive_call:
+        if len(ommers) > 0:
+            # handle ommers of this block
+            for ommer_blockhash in ommers:
+                handle_new_block(ommer_blockhash, recursive_call=True)
+            # also print (possibly re-print!..) parent
+            handle_new_block(parent, recursive_call=True, ommertext='╞')
 
-    # FIXME: inspect ommers!
+    print(blocknum, len(ommers), ommertext, to_hex(blockhash), vote, extratext)
 
     return
 
